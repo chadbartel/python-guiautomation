1c01462679b3e983dc0dd4859892ecb0671d681e
Chad Bartel <chadbartel@users.noreply.github.com>
2016-01-12 12:54:55 -0800

    Update guiautoplayground.py
    Just a place for me to write some practice scripts to refer back to later if I need a refresher.

--- guiautoplayground.py
+++ guiautoplayground.py
@@ -7,9 +7,19 @@ w, h = size()
 print('Width ' + str(w))
 print('Height ' + str(h))
 
-# the following will move your mouse in a square 10 times
-for i in range(10):
+# the following will move your mouse in a square 5 times
+# the coordinates are in an EXPLICIT region of the screen
+for i in range(5):
     moveTo(100, 100, duration=0.25)
     moveTo(200, 100, duration=0.25)
     moveTo(200, 200, duration=0.25)
     moveTo(100, 200, duration=0.25)
+
+# the following will move your mouse in a square 5 times
+# the coordinates are in a RELATIVE region of the screen
+# it's relative to where your mouse is
+for i in range(5):
+    moveRel(100, 0, duration=0.25)
+    moveRel(0, 100, duration=0.25)
+    moveRel(-100, 0, duration=0.25)
+    moveRel(0, -100, duration=0.25)
